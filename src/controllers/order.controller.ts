711e54c1aad4b06348e0e36a91a0d3566327ead6
Vicky AS <vickyhuang808@gmail.com>
2024-08-06 16:13:30 +0700

    order controller create edit

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,5 +1,7 @@
+import OrderModel from '@/models/orders.model';
 import ProductsModel from '@/models/products.model';
 import { IReqProduct, IReqUser } from '@/utils/interfaces';
+import { orderValidation, Yup } from '@/utils/validationSchema';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
@@ -29,7 +31,7 @@ export default {
                         });
                     }
 
-                    const orderedItem : IReqProduct['product'] = {
+                    const orderedItem: IReqProduct['product'] = {
                         name: product?.name,
                         productId: product?._id.toString(),
                         price: product?.price,
@@ -37,17 +39,50 @@ export default {
                     };
 
                     product.qty -= orderItem.quantity;
-                    await product.save({session});
+                    await product.save({ session });
 
                     grandTotal += orderedItem.price * orderedItem.quantity;
                     orderItemsArray.push(orderedItem);
                 }
             }
 
-            
-        } catch (error) {
+            const orderValidated = await orderValidation.validate({
+                grandTotal: grandTotal,
+                createdBy: createdBy,
+                orderItems: orderItemsArray,
+                status: status,
+            });
 
+            const newOrder = await OrderModel.create([orderValidated], { session });
+
+            await session.commitTransaction();
+            session.endSession();
+
+            res.status(201).json({
+                message: "Success create order",
+                data: newOrder,
+            });
+        } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            if (error instanceof Yup.ValidationError) {
+                res.status(400).json({
+                    message: "Fail create order",
+                    detail: error.errors,
+                });
+                return;
+            }
+
+            const err = error as Error;
+
+            res.status(500).json({
+                message: "Fail create order",
+                detail: err.message,
+            });
         }
+    },
+
+    async findAll(req: Request, res: Response) {
 
     }
 };
