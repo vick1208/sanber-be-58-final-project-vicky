36700fe05c43655a3cd3b2ecfa0e7d9b2a2789dd
Vicky AS <vickyhuang808@gmail.com>
2024-08-07 10:32:59 +0700

    update route

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,16 @@ import categoriesController from "./controllers/categories.controller";
 import authController from "./controllers/auth.controller";
 import authMiddleware from "./middlewares/auth.middleware";
 import aclMiddleware from "./middlewares/acl.middleware";
+import orderController from "./controllers/order.controller";
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        status: "OK",
+        message: "Selamat datang di Toserba Typescript",
+    });
+});
 
 router.get("/products", productsController.findAll);
 router.post("/products", productsController.create);
@@ -16,18 +24,20 @@ router.get("/products/:id", productsController.findOne);
 router.put("/products/:id", productsController.update);
 router.delete("/products/:id", productsController.delete);
 
-router.get('/categories',categoriesController.findAll);
-router.post("/categories",categoriesController.create);
-router.get('/categories/:id',categoriesController.findOne);
-router.put('/categories/:id',categoriesController.update);
-router.delete('/categories/:id',categoriesController.delete);
+router.get('/categories', categoriesController.findAll);
+router.post("/categories", categoriesController.create);
+router.get('/categories/:id', categoriesController.findOne);
+router.put('/categories/:id', categoriesController.update);
+router.delete('/categories/:id', categoriesController.delete);
 
-router.post('/auth/login',authController.login);
-router.post('/auth/register',authController.register);
+router.post('/auth/login', authController.login);
+router.post('/auth/register', authController.register);
 // router.get('/auth/me',[authMiddleware,aclMiddleware(["user"])],authController.me); jawaban mini challenge
-router.get('/auth/me',[authMiddleware,aclMiddleware(["admin"])],authController.me); 
+router.get('/auth/me', [authMiddleware, aclMiddleware(["admin"])], authController.me);
+router.post('/auth/profile', authMiddleware, authController.profile);
 
-router.post('/auth/profile',authMiddleware,authController.profile);
+router.get('/orders', authMiddleware, orderController.findAllUserOrder);
+router.post('/orders', authMiddleware, orderController.create);
 
 router.post("/upload", uploadMiddleware.single, uploadController.single);
 router.post("/uploads", uploadMiddleware.multiple, uploadController.multiple);
